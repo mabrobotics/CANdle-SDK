73759248210aa2ca91bb2d3383b8a65c7830f1cd
Piotr Wasilewski <piwasilewski@interia.pl>
2023-09-21 15:23:09 +0200

    wip: testing communication speeds

--- CANdle_lib/src/main.cpp
+++ CANdle_lib/src/main.cpp
@@ -19,6 +19,25 @@ struct CANFrame
 	std::array<uint8_t, 64> payload;
 };
 
+struct StatusFrame
+{
+	struct Statistics
+	{
+		uint8_t averageTxFifoOccupancyPercent;
+		uint8_t averageRxFifoOccupancyPercent;
+		uint8_t maxTxFifoOccupancyPercent;
+		uint8_t maxRxFifoOccupancyPercent;
+	} statistics;
+	uint32_t busStatus;
+};
+
+struct SettingsFrame
+{
+	uint32_t baudrate;
+	uint32_t fdFormat;
+	uint32_t bitRateSwitch;
+};
+
 #pragma pack(pop)
 
 std::unique_ptr<IBusHandler> busHandler;
@@ -33,8 +52,28 @@ IBusHandler::BusFrame makeBusFrameFromCan(CANFrame &canFrame)
 	return usbFrame;
 }
 
+template <typename T, typename Iterator>
+T deserialize(Iterator it)
+{
+	std::array<uint8_t, sizeof(T)> byteArray{};
+	std::copy(it, it + byteArray.size(), byteArray.begin());
+	T frame = std::bit_cast<T>(byteArray);
+	return frame;
+}
+
+template <typename T, typename Iterator>
+void serialize(T frame, Iterator it)
+{
+	std::array<uint8_t, sizeof(T)> byteArray{};
+	byteArray = std::bit_cast<decltype(byteArray)>(frame);
+	std::copy(byteArray.begin(), byteArray.end(), it);
+}
+
 void processDataThread()
 {
+
+	std::array<uint32_t, 10> stats{1};
+
 	while (1)
 	{
 		auto frame = busHandler->getFromFifo();
@@ -48,52 +87,84 @@ void processDataThread()
 			auto canHeader = getHeaderFromArray<CANFrame::Header>(it);
 			it += sizeof(canHeader);
 
-			std::cout << "ID: " << (int)canHeader.canId << " Len: " << (int)canHeader.length << " Data: ";
-
-			for (int i = 0; i < canHeader.length; i++)
-				std::cout << std::hex << " 0x" << (int)*it++ << " ";
-			std::cout << std::endl;
+			// std::cout << "ID: " << (int)canHeader.canId << " Len: " << (int)canHeader.length << " Data: ";
+			// for (int i = 0; i < canHeader.length; i++)
+			// 	std::cout << std::hex << " 0x" << (int)*it++ << " ";
+			// std::cout << std::endl;
+		}
+		else if (frame->header.id == 0x02)
+		{
+			auto status = deserialize<StatusFrame>(frame->payload.begin());
+			std::cout << "CAN Status: " << (int)status.statistics.averageRxFifoOccupancyPercent << " "
+					  << (int)status.statistics.averageTxFifoOccupancyPercent << " "
+					  << (int)status.statistics.maxRxFifoOccupancyPercent << " "
+					  << (int)status.statistics.maxTxFifoOccupancyPercent << " "
+					  << (int)status.busStatus << std::endl;
 		}
 	}
 }
 
 void commandDataThread()
 {
-	CANFrame canFrame;
+	CANFrame SYNC{};
+	SYNC.header.canId = 0x080;
+	SYNC.header.length = 0;
+
+	CANFrame canFrame{};
 	canFrame.header.canId = 0x601;
 	canFrame.header.length = 8;
 	canFrame.payload = {0x40, 0xC5, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-	CANFrame canFrame2;
+	CANFrame canFrame2{};
 	canFrame2.header.canId = 0x602;
 	canFrame2.header.length = 8;
 	canFrame2.payload = {0x40, 0x64, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-	CANFrame RPDO3_1;
-	RPDO3_1.header.canId = 0x402;
-	RPDO3_1.header.length = 6;
-	RPDO3_1.payload = {0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	std::array<CANFrame, 12> RPDOs{};
+	std::array<IBusHandler::BusFrame, 12> RPDOFrames{};
 
-	CANFrame RPDO3_2;
-	RPDO3_2.header.canId = 0x401;
-	RPDO3_2.header.length = 6;
-	RPDO3_2.payload = {0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+	for (uint32_t i = 0; i < RPDOs.size(); i++)
+	{
+		RPDOs[i].header.canId = 0x400 + i + 1;
+		RPDOs[i].header.length = 12;
+
+		RPDOFrames[i] = makeBusFrameFromCan(RPDOs[i]);
+	}
+
+	IBusHandler::BusFrame usbFrame{};
+	usbFrame.header.id = 0x03;
+	usbFrame.header.length = 1;
+	usbFrame.payload.at(0) = 0x03;
+	busHandler->addToFifo(usbFrame);
+
+	IBusHandler::BusFrame usbSettingsFrame{};
+	usbSettingsFrame.header.id = 0x04;
+	usbSettingsFrame.header.length = sizeof(SettingsFrame);
+	SettingsFrame settings{};
+	settings.baudrate = 8000000;
+	settings.bitRateSwitch = 0x00100000U;
+	settings.fdFormat = 0x00200000U;
+	serialize(settings, usbSettingsFrame.payload.begin());
+	busHandler->addToFifo(usbSettingsFrame);
+
+	auto startTime = std::chrono::steady_clock::now();
+	const std::chrono::microseconds interval(2000);
+
+	auto sync = makeBusFrameFromCan(SYNC);
 
 	while (1)
 	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		auto elapsedTime = std::chrono::steady_clock::now() - startTime;
 
-		auto frame1 = makeBusFrameFromCan(canFrame);
-		auto frame2 = makeBusFrameFromCan(canFrame2);
+		if (elapsedTime > interval)
+		{
+			startTime = std::chrono::steady_clock::now();
 
-		auto frame3 = makeBusFrameFromCan(RPDO3_1);
-		auto frame4 = makeBusFrameFromCan(RPDO3_2);
+			for (uint32_t i = 0; i < 12; i++)
+				busHandler->addToFifo(RPDOFrames[i]);
 
-		busHandler->addToFifo(frame1);
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		busHandler->addToFifo(frame2);
-		busHandler->addToFifo(frame3);
-		busHandler->addToFifo(frame4);
+			busHandler->addToFifo(sync);
+		}
 	}
 }
 
