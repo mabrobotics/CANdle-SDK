3b47bbc9e90e085b545681c271f972e9d107092f
Piotr Wasilewski <piwasilewski@interia.pl>
2024-03-04 09:17:54 +0100

    feature: updated simple movement example

--- Examples/simple_movement.cpp
+++ Examples/simple_movement.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv)
 {
-	const uint32_t id = 1;
+	const uint32_t id = 2;
 
 	Candle candle;
 
@@ -22,13 +22,11 @@ int main(int argc, char** argv)
 
 	auto md80 = candle.getMd80(id);
 
-	candle.setSendSync(true, 2000);
-
 	float x = 0;
 
 	while (1)
 	{
-		candle.writeSDO(id, 0x607A, 0x0, (uint32_t)(10000.0f * sin(x)));
+		candle.writeSDO(id, 0x607A, 0x0, (int32_t)(10000.0f * sin(x)));
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		x += 0.01;
 	}
