ce148c9739eaea180a233f60a8f8c037a2ddbd81
Jakub Matyszczak <jakub.matyszczak@gmail.com>
2024-06-11 08:43:23 +0200

    feat: readme and scripts for udev rules and building

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# CANdle-SDK
+A toolkit for developing applications using MAB Robotics MD motor controllers and actuators.
+
+## Dependencies
+### Linux
+```
+sudo apt install libusb-1.0-0-dev
+```
+
+## Build
+```
+./launch/buildForLinux.sh
+```
---
+++ launch/buildForLinux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BUILD_DIRECTORY=build/linux/
+start_dir=$(pwd)
+base_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE}")")")"
+cd ${base_dir}
+
+mkdir $BUILD_DIRECTORY -p
+cd $BUILD_DIRECTORY
+cmake ../..
+make -j4
+cpack -G DEB -DCPACK_INSTALL_CMAKE_PROJECTS=ALL
+
+cd ${start_dir}
