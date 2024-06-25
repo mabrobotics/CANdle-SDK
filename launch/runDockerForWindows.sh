a515b75f43bd90b4e1422be5cabfbfe5ec67679e
Jakub Matyszczak <jakub.matyszczak@mabrobotics.pl>
2024-06-25 11:44:23 +0200

    feat: updated docker scrupts. tested under windows

--- launch/runDockerForWindows.sh
+++ launch/runDockerForWindows.sh
@@ -4,9 +4,23 @@ start_dir=$(pwd)
 base_dir="$(dirname "$(readlink -f "${BASH_SOURCE}")")"
 cd ${base_dir}
 
+image=candle-sdk-win:v1
+
+docker image inspect ${image} &> /dev/null
+inspect=$?
+if [ ${inspect} != 0 ]; then
+    echo "${image} not found locally."
+    read -p "Build it? [y/n]" -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]] then
+        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    fi
+    docker build ./WindowsDocker/ -t ${image}
+fi
+
 docker run -u root \
     -v "$(pwd)/..":"/candle-sdk" \
-    candle-sdk-win:v1 \
+    ${image} \
     /bin/bash -c "cd /candle-sdk && ./launch/buildForWindows.sh"
 
 cd ${start_dir}
