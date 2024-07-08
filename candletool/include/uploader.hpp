669fea32d1c8ff2a8b67b5ff88d7791a7be11093
mkowalski <maciej.kowalski@mabrobotics.pl>
2024-07-08 13:28:34 +0200

    improve logging

--- candletool/include/uploader.hpp
+++ candletool/include/uploader.hpp
@@ -7,64 +7,65 @@
 namespace mab
 {
 
-	/**
-	 * @class FirmwareUploader
-	 * @brief Class for uploading firmware to a device.
-	 *
-	 * The FirmwareUploader class provides functionality to upload firmware to a device.
-	 * It is used in conjunction with the Candle class to perform firmware updates.
-	 */
-	class FirmwareUploader
-	{
-	  public:
-		/**
-		 * @brief Constructs a FirmwareUploader object.
-		 * @param _candle The reference to the Candle object.
-		 * @param mabFile The path to the MAB file containing the firmware.
-		 */
-		FirmwareUploader(Candle& _candle, const std::string& mabFile);
+    /**
+     * @class FirmwareUploader
+     * @brief Class for uploading firmware to a device.
+     *
+     * The FirmwareUploader class provides functionality to upload firmware to a device.
+     * It is used in conjunction with the Candle class to perform firmware updates.
+     */
+    class FirmwareUploader
+    {
+      public:
+        /**
+         * @brief Constructs a FirmwareUploader object.
+         * @param _candle The reference to the Candle object.
+         * @param mabFile The path to the MAB file containing the firmware.
+         */
+        FirmwareUploader(Candle& _candle, const std::string& mabFile);
 
-		/**
-		 * @brief Performs the firmware update.
-		 *
-		 * @param id The ID of the device to update.
-		 * @param directly If true, connect to bootloader directly. If false, connect to bootloader
-		 * via the application.
-		 * @return true
-		 * @return false
-		 */
-		bool flashDevice(int id, bool directly);
-		void setVerbosity(bool verbosity);
+        /**
+         * @brief Performs the firmware update.
+         *
+         * @param id The ID of the device to update.
+         * @param directly If true, connect to bootloader directly. If false, connect to bootloader
+         * via the application.
+         * @return true
+         * @return false
+         */
+        bool flashDevice(int id, bool directly);
+        void setVerbosity(bool verbosity);
 
-	  private:
-		mab::Candle& candle;
-		logger		 log;
+      private:
+        mab::Candle& candle;
+        logger       log;
 
-		enum BootloaderFrameId_t : uint8_t
-		{
-			CMD_TARGET_RESET = 0x13,
-			CMD_HOST_INIT	 = 0xA1,
-			CMD_PAGE_PROG	 = 0xA2,
-			CMD_BOOT		 = 0xA3,
-			CMD_WRITE		 = 0xA4,
-		};
+        enum BootloaderFrameId_t : uint8_t
+        {
+            CMD_TARGET_RESET = 0x13,
+            CMD_HOST_INIT    = 0xA1,
+            CMD_PAGE_PROG    = 0xA2,
+            CMD_BOOT         = 0xA3,
+            CMD_WRITE        = 0xA4,
+        };
 
-		static constexpr size_t	  M_PAGE_SIZE	 = 2048;
-		static constexpr size_t	  M_CHUNK_SIZE	 = 64;
-		static constexpr uint32_t M_BOOT_ADDRESS = 0x08005000;
+        static constexpr size_t   M_PAGE_SIZE    = 2048;
+        static constexpr size_t   M_CHUNK_SIZE   = 64;
+        static constexpr uint32_t M_BOOT_ADDRESS = 0x08005000;
 
-		BinaryParser m_mabFile;
-		size_t		 m_fileSize;
-		size_t		 m_bytesToUpload;
-		size_t		 m_pagesToUpload;
-		uint32_t	 m_currentPage;
-		uint32_t	 m_currentId;
+        BinaryParser m_mabFile;
+        size_t       m_fileSize;
+        size_t       m_bytesToUpload;
+        size_t       m_pagesToUpload;
+        uint32_t     m_currentPage;
+        uint32_t     m_currentId;
 
-		void sendResetCmd();
-		bool sendInitCmd();
-		bool sendPageProgCmd();
-		bool sendPage();
-		bool sendWriteCmd(uint8_t* pPageBuffer, int bufferSize);
-		bool sendBootCmd();
-	};
+        void sendResetCmd();
+        bool sendInitCmd();
+        bool sendPageProgCmd();
+        bool sendPage();
+        bool sendWriteCmd(uint8_t* pPageBuffer, int bufferSize);
+        bool sendBootCmd();
+        void printProgress(double percentage);
+    };
 }  // namespace mab
--- commons/logger/logger.cpp
+++ commons/logger/logger.cpp
@@ -3,59 +3,59 @@
 
 void logger::info(const char* msg, ...)
 {
-	if (level > LogLevel_E::INFO)
-		return;
-	va_list args;
-	va_start(args, msg);
-	fprintf(stderr, "[%s][" BLUE "INFO" RESETCLR "] ", this->tag.c_str());
-	vfprintf(stderr, msg, args);
-	printf("\n");
-	va_end(args);
+    if (level > LogLevel_E::INFO)
+        return;
+    va_list args;
+    va_start(args, msg);
+    fprintf(stderr, "[" BLUE "INFO" RESETCLR "][%s]", this->tag.c_str());
+    vfprintf(stderr, msg, args);
+    printf("\n");
+    va_end(args);
 }
 void logger::success(const char* msg, ...)
 {
-	if (level > LogLevel_E::INFO)
-		return;
-	va_list args;
-	va_start(args, msg);
-	fprintf(stderr, "[%s][" GREEN " OK " RESETCLR "] ", this->tag.c_str());
-	vfprintf(stderr, msg, args);
-	printf("\n");
-	va_end(args);
+    if (level > LogLevel_E::INFO)
+        return;
+    va_list args;
+    va_start(args, msg);
+    fprintf(stderr, "[" GREEN " OK " RESETCLR "][%s]", this->tag.c_str());
+    vfprintf(stderr, msg, args);
+    printf("\n");
+    va_end(args);
 }
 
 void logger::debug(const char* msg, ...)
 {
-	if (level > LogLevel_E::DEBUG)
-		return;
-	va_list args;
-	va_start(args, msg);
-	fprintf(stderr, "[%s][" ORANGE "DBG " RESETCLR "]", this->tag.c_str());
-	vfprintf(stderr, msg, args);
-	printf("\n");
-	va_end(args);
+    if (level > LogLevel_E::DEBUG)
+        return;
+    va_list args;
+    va_start(args, msg);
+    fprintf(stderr, "[" ORANGE "DBG " RESETCLR "][%s]", this->tag.c_str());
+    vfprintf(stderr, msg, args);
+    printf("\n");
+    va_end(args);
 }
 
 void logger::warn(const char* msg, ...)
 {
-	if (level > LogLevel_E::WARN)
-		return;
-	va_list args;
-	va_start(args, msg);
-	fprintf(stderr, "[%s][" YELLOW "WARN" RESETCLR "] ", this->tag.c_str());
-	vfprintf(stderr, msg, args);
-	printf("\n");
-	va_end(args);
+    if (level > LogLevel_E::WARN)
+        return;
+    va_list args;
+    va_start(args, msg);
+    fprintf(stderr, "[" YELLOW "WARN" RESETCLR "][%s]", this->tag.c_str());
+    vfprintf(stderr, msg, args);
+    printf("\n");
+    va_end(args);
 }
 
 void logger::error(const char* msg, ...)
 {
-	if (level > LogLevel_E::ERROR)
-		return;
-	va_list args;
-	va_start(args, msg);
-	fprintf(stderr, "[%s][" RED "ERR " RESETCLR "] ", this->tag.c_str());
-	vfprintf(stderr, msg, args);
-	fprintf(stderr, "\n");
-	va_end(args);
+    if (level > LogLevel_E::ERROR)
+        return;
+    va_list args;
+    va_start(args, msg);
+    fprintf(stderr, "[" RED "ERR " RESETCLR "][%s]", this->tag.c_str());
+    vfprintf(stderr, msg, args);
+    fprintf(stderr, "\n");
+    va_end(args);
 }
