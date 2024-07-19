19f269287209af7c219fd68c830463117c92c2f9
PawelKorsak <p.korsak010@gmail.com>
2024-07-19 15:23:16 +0200

    feature: working build system of pycandle

--- launch/pythonBuildWheel.sh
+++ launch/pythonBuildWheel.sh
@@ -9,13 +9,15 @@ if [ "$NAME" != "CANdle-SDK" ]; then
     echo "SCRIPT NEEDS TO BE RUN FROM CANDLE-SDK'S ROOT DIRECTORY"
 fi
 
-if pipx run build ; then
-    echo "Built pycandle successfuly"
-else
-    echo -e "${RED}Error in building pycandle${ENDCOLOR}"
-    exit 1
-fi
+# if pipx run build ; then
+#     echo "Built pycandle successfuly"
+# else
+#     echo -e "${RED}Error in building pycandle${ENDCOLOR}"
+#     exit 1
+# fi
 
+
+# cibuildwheel will pre-compile against many python APIs and libc distributions
 if pipx run cibuildwheel --platform linux --archs x86_64 ; then
     echo "Cross-compiled pycandle successfuly"
 else
