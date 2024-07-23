1c5e20af03e9a966da0f7da45734cc1d91f1edb8
PawelKorsak <p.korsak010@gmail.com>
2024-07-23 13:19:03 +0200

    fix: changed old build functions

--- launch/pythonBuildWheel.sh
+++ launch/pythonBuildWheel.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-NAME=${PWD##*/}  # retain the part after the last slash
 
 RED="\e[31m"
 ENDCOLOR="\e[0m"
 
+start_dir=$(pwd)
+cd "$(dirname "$0")"
+cd ..
+
+NAME=${PWD##*/}
+
 if [ "$NAME" != "CANdle-SDK" ]; then
     echo "SCRIPT NEEDS TO BE RUN FROM CANDLE-SDK'S ROOT DIRECTORY"
+    cd ${start_dir}
+    exit 1
 fi
 
 # if pipx run build ; then
@@ -22,5 +29,8 @@ if pipx run cibuildwheel --platform linux --archs x86_64 ; then
     echo "Cross-compiled pycandle successfuly"
 else
     echo -e "${RED}Error in cross-building pycandle${ENDCOLOR}"
+    cd ${start_dir}
     exit 1
 fi
+
+cd ${start_dir}
