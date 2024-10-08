461418188d093914d99c57ca2c195f15078b03c7
PawelKorsak <p.korsak010@gmail.com>
2024-10-08 09:12:42 +0200

    fix: multiple definitions

--- commons/shared_data/manufacturer_data.hpp
+++ commons/shared_data/manufacturer_data.hpp
@@ -5,14 +5,14 @@
 namespace mab
 {
 
-    enum class deviceType : u8{
-        UNKNOWN = 0x00,
+    enum class deviceType_E : u8{
+        UNKNOWN_DEVICE = 0x00,
         MD80    = 0x01, 
         MD20    = 0x02,
         MD80HV  = 0x03,
         PDS     = 0x04,
 
-        UNKNOWN    = 0xA0,
+        UNKNOWN_DEVICE_2    = 0xA0,
         CANDLE     = 0xA1,
         CANDLE_HAT = 0xA2
     };
@@ -21,7 +21,7 @@ namespace mab
 
     struct hardwareType_S
     {
-        deviceType deviceType;  // 0xAX - hosts, 0x0X - peripherials
+        deviceType_E deviceType;  // 0xAX - hosts, 0x0X - peripherials
         u8 deviceRevision;
     };
 
