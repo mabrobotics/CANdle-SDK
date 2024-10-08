a8d3edf1b910d4db981078e3543de386b6c87a61
PawelKorsak <p.korsak010@gmail.com>
2024-10-08 09:03:49 +0200

    feature: added device type enum

--- commons/shared_data/manufacturer_data.hpp
+++ commons/shared_data/manufacturer_data.hpp
@@ -5,11 +5,23 @@
 namespace mab
 {
 
+    enum class deviceType : u8{
+        UNKNOWN = 0x00,
+        MD80    = 0x01, 
+        MD20    = 0x02,
+        MD80HV  = 0x03,
+        PDS     = 0x04,
+
+        UNKNOWN    = 0xA0,
+        CANDLE     = 0xA1,
+        CANDLE_HAT = 0xA2
+    };
+
 #pragma pack(push, 1)
 
     struct hardwareType_S
     {
-        u8 deviceType;  // 0xAX - hosts, 0x0X - peripherials
+        deviceType deviceType;  // 0xAX - hosts, 0x0X - peripherials
         u8 deviceRevision;
     };
 
