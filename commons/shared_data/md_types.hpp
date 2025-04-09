d4826ae5585e05209b9942322336f4843f2e1510
PawelKorsak <p.korsak010@gmail.com>
2025-04-09 10:52:08 +0200

    feature: constexpr constructors

--- commons/shared_data/md_types.hpp
+++ commons/shared_data/md_types.hpp
@@ -352,26 +352,10 @@ namespace mab
             std::apply([&](auto&&... regs) { (func(regs), ...); }, regs.getAllRegisters());
         }
 
-        template <typename T>
-        std::optional<std::reference_wrapper<MDRegisterEntry_S<T>&>> findRegisterByAddress(
-            u16 targetAddress)
+        template <class F>
+        constexpr void compileTimeForEachRegister(F&& func)
         {
-            std::optional<std::reference_wrapper<MDRegisterEntry_S<T>&>> result;
-
-            forEachRegister(this,
-                            [&](auto& reg)
-                            {
-                                if (reg.m_regAddress == targetAddress && !result.has_value())
-                                {
-                                    if constexpr (std::is_same_v<std::decay_t<decltype(reg)>,
-                                                                 MDRegisterEntry_S<T>>)
-                                    {
-                                        result = &reg;
-                                    }
-                                }
-                            });
-
-            return result;
+            std::apply([&](auto&&... regs) { (func(regs), ...); }, getAllRegisters());
         }
     };
 
