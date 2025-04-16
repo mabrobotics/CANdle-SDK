2617102b8e58c2336c52c4806f9e22290074eb31
PawelKorsak <p.korsak010@gmail.com>
2025-04-16 11:02:53 +0200

    feature: working impedance mode example

--- examples/py/md_impledance.py
+++ examples/py/md_impledance.py
@@ -1,4 +1,19 @@
-import pyCandleMAB
+import pyCandle as pc
+import time
 
+candle = pc.attachCandle(pc.CANdleBaudrate_E.CAN_BAUD_1M, pc.busTypes_t.USB)
 
-candle = attachCandle(CANdleBaudrate_E.CAN_BAUD_1M, busTypes_t.USB)
+md = pc.MD(100, candle)
+
+err = md.init()
+
+print(err)
+
+if err == pc.MD_Error_t.OK:
+    md.zero()
+    md.setMotionMode(pc.MotionMode_t.IMPEDANCE)
+    md.enable()
+    for i in range(100):
+        md.setTargetPosition(i * 0.05)
+        time.sleep(0.1)
+    md.disable()
--- pycandle/src/bindings.cpp
+++ pycandle/src/bindings.cpp
@@ -5,12 +5,12 @@
 #include <pybind11/stl.h>
 
 #include <cstdint>
+#include <memory>
 #include <vector>
 
-#include "I_communication_interface.hpp"
 #include "candle_types.hpp"
 #include "candle_v2.hpp"
-#include "register.hpp"
+#include "MD.hpp"
 
 namespace py = pybind11;
 
@@ -21,81 +21,18 @@ namespace mab
         return attachCandle(baudrate, busType);
     }
 
-    // class CandlePython : public Candle
-    // {
-    //   public:
-    //     CandlePython(CANdleBaudrate_E  canBaudrate,
-    //                  bool              printVerbose = true,
-    //                  mab::BusType_E    busType      = BusType_E::USB,
-    //                  const std::string device       = "")
-    //         : Candle(canBaudrate, printVerbose, busType, device)
-    //     {
-    //     }
-    //     CandlePython(CANdleBaudrate_E canBaudrate, bool printVerbose, std::shared_ptr<Bus> bus)
-    //         : Candle(canBaudrate, printVerbose, bus)
-    //     {
-    //     }
-    //     virtual ~CandlePython() = default;
+    MD createMD(int canId, std::shared_ptr<CandleV2> candle)
+    {
+        return MD(canId, candle.get());
+    }
 
-    //     float readMd80Register_(uint16_t canId, Md80Reg_E regId, float regValue)
-    //     {
-    //         (void)regValue;
-    //         md80Register->read(canId, regId, regValue);
-    //         return regValue;
-    //     }
-
-    //     int64_t readMd80Register_(uint16_t canId, Md80Reg_E regId, int64_t regValue)
-    //     {
-    //         (void)regValue;
-    //         int64_t regValue_i64 = 0;
-
-    //         md80Register->read(canId, regId, regValue_i64);
-
-    //         // if (md80Register->getType(regId) == Register::type::I8)
-    //         // 	return *(uint8_t*)&regValue_i64;
-    //         // else if (md80Register->getType(regId) == Register::type::I16)
-    //         // 	return *(uint16_t*)&regValue_i64;
-    //         // else if (md80Register->getType(regId) == Register::type::I32)
-    //         // 	return *(uint32_t*)&regValue_i64;
-    //         // else
-    //         return regValue_i64;
-    //     }
-
-    //     std::string readMd80Register_(uint16_t canId, Md80Reg_E regId, char* regValue)
-    //     {
-    //         (void)regValue;
-    //         char regValue_[64] = {0};
-    //         md80Register->read(canId, regId, regValue_);
-    //         return std::string(regValue_);
-    //     }
-
-    //     bool writeMd80Register_(uint16_t canId, Md80Reg_E regId, float regValue)
-    //     {
-    //         return md80Register->write(canId, regId, regValue);
-    //     }
-
-    //     bool writeMd80Register_(uint16_t canId, Md80Reg_E regId, uint32_t regValue)
-    //     {
-    //         return md80Register->write(canId, regId, regValue);
-    //     }
-
-    //     bool writeMd80Register_(uint16_t canId, Md80Reg_E regId, int32_t regValue)
-    //     {
-    //         return md80Register->write(canId, regId, regValue);
-    //     }
-
-    //     bool writeMd80Register_(uint16_t canId, Md80Reg_E regId, std::string regValue)
-    //     {
-    //         char regValue_[64] = {0};
-    //         memcpy(regValue_, regValue.c_str(), regValue.length());
-    //         return md80Register->write(canId, regId, regValue_);
-    //     }
-    // };
 }  // namespace mab
 
 PYBIND11_MODULE(pyCandle, m)
 {
     m.doc() = "pyCandle module for interfacing with md80 drives using Python";
+
+    // CANdle class
 
     py::enum_<mab::CANdleBaudrate_E>(m, "CANdleBaudrate_E")
         .value("CAN_BAUD_1M", mab::CAN_BAUD_1M)
@@ -127,7 +64,42 @@ PYBIND11_MODULE(pyCandle, m)
           &mab::pyAttachCandle,
           py::arg("baudrate"),
           py::arg("busType"),
-          py::return_value_policy::reference,
+          py::return_value_policy::take_ownership,
           "Attach a CANdle device to the system.");
-    // py::class_<mab::CandleV2>(m, "CandleV2").def();
+
+    py::class_<mab::CandleV2>(m, "CandleV2");
+
+    // MD class
+    py::enum_<mab::MD::Error_t>(m, "MD_Error_t")
+        .value("OK", mab::MD::Error_t::OK)
+        .value("REQUEST_INVALID", mab::MD::Error_t::REQUEST_INVALID)
+        .value("TRANSFER_FAILED", mab::MD::Error_t::TRANSFER_FAILED)
+        .value("NOT_CONNECTED", mab::MD::Error_t::NOT_CONNECTED)
+        .export_values();
+
+    py::enum_<mab::Md80Mode_E>(m, "MotionMode_t")
+        .value("IDLE", mab::Md80Mode_E::IDLE)
+        .value("POSITION_PID", mab::Md80Mode_E::POSITION_PID)
+        .value("VELOCITY_PID", mab::Md80Mode_E::VELOCITY_PID)
+        .value("RAW_TORQUE", mab::Md80Mode_E::RAW_TORQUE)
+        .value("IMPEDANCE", mab::Md80Mode_E::IMPEDANCE)
+        .export_values();
+
+    py::class_<mab::MD>(m, "MD")
+        .def(py::init([](int canId, mab::CandleV2* candle) -> auto
+                      { return mab::MD(canId, candle); }))
+        .def("init", &mab::MD::init, "Initialize the MD device. Returns an error if not connected.")
+        .def("blink", &mab::MD::blink, "Blink the built-in LEDs.")
+        .def("enable", &mab::MD::enable, "Enable PWM output of the drive.")
+        .def("disable", &mab::MD::disable, "Disable PWM output of the drive.")
+        .def("reset", &mab::MD::reset, "Reset the driver.")
+        .def("zero", &mab::MD::zero, "Zero out the position of the encoder.")
+        .def("setMotionMode",
+             &mab::MD::setMotionMode,
+             py::arg("mode"),
+             "Set the motion mode of the MD device.")
+        .def("setTargetPosition",
+             &mab::MD::setTargetPosition,
+             py::arg("position"),
+             "Set the target position of the MD device.");
 }
