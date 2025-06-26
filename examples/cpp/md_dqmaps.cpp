c691e82f628e54ada3343e5a45626a360e31f8f8
Jakub Matyszczak <jakub.matyszczak@mabrobotics.pl>
2025-06-26 12:54:48 +0200

    feat: dq maps setup

--- examples/cpp/md_dqmaps.cpp
+++ examples/cpp/md_dqmaps.cpp
@@ -7,6 +7,9 @@ constexpr u32 nVoltage = 5;
 constexpr u32 nRows    = 17;
 constexpr u32 nCols    = 15;
 f32           maps[2][nVoltage][nRows][nCols];
+f32           voltages[5]    = {36, 42, 48, 54, 60};
+f32           torques[17]    = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+f32           velocities[15] = {0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70};
 
 bool read_csv(char filepath[]);
 
@@ -33,6 +36,31 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
     mab::MDRegisters_S regs;
+    memcpy(regs.mapVoltageValues.value, voltages, 5 * sizeof(f32));
+    memcpy(regs.mapTorqueValues0.value, &torques[0], 15 * sizeof(f32));
+    memcpy(regs.mapTorqueValues1.value, &torques[15], 2 * sizeof(f32));
+    memcpy(regs.mapVelocityValues.value, velocities, 15 * sizeof(f32));
+    if (md.writeRegisters(regs.mapVoltageValues) != mab::MD::Error_t::OK ||
+        md.writeRegisters(regs.mapTorqueValues0) != mab::MD::Error_t::OK ||
+        md.writeRegisters(regs.mapTorqueValues1) != mab::MD::Error_t::OK ||
+        md.writeRegisters(regs.mapVelocityValues) != mab::MD::Error_t::OK)
+    {
+        fprintf(stderr, "Failed to set up map!\n");
+        return EXIT_FAILURE;
+    }
+    fprintf(stderr, "voltages: ");
+    for (int i = 0; i < 5; i++)
+        fprintf(stderr, "%.1f, ", regs.mapVoltageValues.value[i]);
+    fprintf(stderr, "\n");
+    fprintf(stderr, "torques: ");
+    for (int i = 0; i < 17; i++)
+        fprintf(stderr, "%.1f, ", torques[i]);
+    fprintf(stderr, "\n");
+    fprintf(stderr, "velocities: ");
+    for (int i = 0; i < 5; i++)
+        fprintf(stderr, "%.1f, ", velocities[i]);
+    fprintf(stderr, "\n");
+
     for (u32 dq = 0; dq < 2; dq++)
         for (u32 volt = 0; volt < nVoltage; volt++)
             for (u32 row = 0; row < nRows; row++)
@@ -44,22 +72,36 @@ int main(int argc, char** argv)
                 bool selectOk = md.writeRegisters(regs.mapSelectRow) == mab::MD::Error_t::OK;
                 bool dataOk   = md.writeRegisters(regs.mapRowData) == mab::MD::Error_t::OK;
                 memset(regs.mapRowData.value, 0, 60);
-                bool readOk   = md.readRegister(regs.mapRowData) == mab::MD::Error_t::OK;
+                bool readOk = md.readRegister(regs.mapRowData) == mab::MD::Error_t::OK;
                 if (readOk)
                     for (u32 i = 0; i < nCols; i++)
                     {
                         f32 ptr = (regs.mapRowData.value)[i];
                         readOk &= maps[dq][volt][row][i] == ptr;
                     }
-                fprintf(stderr,
-                        "%s %d %d = select: %s, data: %s, verify: %s\n",
-                        dq == 0 ? "ID" : "ID",
-                        volt,
-                        row,
-                        selectOk ? "OK" : "NOK",
-                        dataOk ? "OK" : "NOK",
-                        readOk ? "OK" : "NOK");
+                if (!selectOk || !dataOk || !readOk)
+                {
+                    fprintf(stderr,
+                            "%s %d %d = select: %s, data: %s, verify: %s\n",
+                            dq == 0 ? "ID" : "ID",
+                            volt,
+                            row,
+                            selectOk ? "OK" : "NOK",
+                            dataOk ? "OK" : "NOK",
+                            readOk ? "OK" : "NOK");
+                    fprintf(stderr, "Failed to set up map values!\n");
+                    return EXIT_FAILURE;
+                }
             }
+    fprintf(stderr, "Enable Maps? [y/n] ");
+    if (fgetc(stdin) != 'y')
+        return EXIT_SUCCESS;
+    regs.mapUseId = true;
+    regs.mapUseIq = true;
+    if (md.writeRegisters(regs.mapUseIq) != mab::MD::Error_t::OK ||
+        md.writeRegisters(regs.mapUseId) != mab::MD::Error_t::OK)
+        fprintf(stderr, "Failed to enable Maps!\n");
+    return EXIT_FAILURE;
 }
 
 bool read_csv(char filepath[])
@@ -90,16 +132,20 @@ bool read_csv(char filepath[])
                     return false;
                 }
                 char* delimiter = buf;
-                for (int i = 3; i < csvColumns; i++)
+                for (int i = 0; i < csvColumns; i++)
                 {
-                    char* substring            = delimiter;
-                    delimiter                  = strchr(delimiter, ',');
-                    *delimiter                 = 0;
-                    maps[dq][volt][row][i - 3] = atof(substring);
+                    char* substring = delimiter;
+                    delimiter       = strchr(delimiter, ',');
+                    if (delimiter == nullptr)
+                        delimiter = strchr(substring, '\n');
+                    *delimiter = 0;
                     delimiter++;
-                    fprintf(stderr, "%.2f,", maps[dq][volt][row][i - 3]);
+                    if (i < 3)  // ignore first 3 columns
+                        continue;
+                    maps[dq][volt][row][i - 3] = atof(substring);
+                    // fprintf(stderr, "%.2f,", maps[dq][volt][row][i - 3]);
                 }
-                fprintf(stderr, "\n");
+                // fprintf(stderr, "\n");
             }
 
     return true;
