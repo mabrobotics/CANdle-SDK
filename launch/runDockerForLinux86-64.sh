b42d9851456101476469b80a3c719dcc18a8a3a4
PawelKorsak <p.korsak010@gmail.com>
2025-07-02 12:07:55 +0200

    feature: made main image more minimalistic

---
+++ launch/runDockerForLinux86-64.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+start_dir=$(pwd)
+base_dir="$(dirname "$(readlink -f "${BASH_SOURCE}")")"
+cd ${base_dir}
+
+image=mab_build_environment:v1 # Change to v1 when available
+
+docker image inspect ${image} &> /dev/null
+inspect=$?
+if [ ${inspect} != 0 ]; then
+    echo "${image} not found locally."
+    docker pull ${image}
+fi
+
+if docker run \
+    -u root \
+    -v "$(pwd)/..":"/candle-sdk" \
+    ${image} \
+    /bin/bash -c "cd /candle-sdk && ./launch/buildForLinux.sh" ; then
+    echo "Build successful."
+else
+    echo "Build failed."
+    exit 1
+fi
+
+cd ${start_dir}
