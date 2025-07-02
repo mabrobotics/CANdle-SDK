ebf642511f6836a53cbabdc2560216df7b406ced
PawelKorsak <p.korsak010@gmail.com>
2025-07-02 13:45:23 +0200

    fix: unified build scripts

--- launch/buildForLinux.sh
+++ launch/buildForLinux.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-BUILD_ROOT_DIR=./build
-BUILD_DIRECTORY=${BUILD_ROOT_DIR}/linux/
+set -e
+BUILD_DIRECTORY=build
 start_dir=$(pwd)
 base_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE}")")")"
 cd ${base_dir}
 
 mkdir $BUILD_DIRECTORY -p
 cd $BUILD_DIRECTORY
-cmake ../..
+chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
+cmake ..
+chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
 make -j4
+chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
 cpack -G DEB
-chmod -R a+rw ${base_dir}/$BUILD_ROOT_DIR
+chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
 
 cd ${start_dir}
--- launch/buildForWindows.sh
+++ launch/buildForWindows.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-BUILD_ROOT_DIR=./build
-BUILD_DIRECTORY=${BUILD_ROOT_DIR}/linux/
+
+set -e
+BUILD_DIRECTORY=build
 start_dir=$(pwd)
 base_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE}")")")" # CANdle SDK directory
 cd ${base_dir}
 
 mkdir $BUILD_DIRECTORY -p
 cd $BUILD_DIRECTORY
-cmake ../../ -DPLATFORM="WIN"
+chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
+cmake .. -DPLATFORM="WIN"
+chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
 make -j
+chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
 cpack -G NSIS
-chmod -R a+rw ${base_dir}/$BUILD_ROOT_DIR
+chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
 
 cd ${start_dir}
