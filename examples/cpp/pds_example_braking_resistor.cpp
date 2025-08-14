8584f88b57663f73f95007eefa57d234bf3ec362
Amronn <zarkadiusz18@gmail.com>
2025-08-14 11:45:15 +0200

    feat: Add PDS examples

---
+++ examples/cpp/pds_example_braking_resistor.cpp
@@ -0,0 +1,50 @@
+/*
+    MAB Robotics
+
+    Power Distribution System Example
+
+    Power stage and braking resistor
+*/
+
+#include "candle.hpp"
+#include "pds.hpp"
+
+using namespace mab;
+
+constexpr u16 PDS_CAN_ID = 120;
+
+constexpr socketIndex_E BRAKE_RESISTOR_SOCKET_INDEX = socketIndex_E::SOCKET_3;
+
+int main()
+{
+    Logger _log;
+    _log.m_tag = "PDS Example";
+
+    auto candle = attachCandle(CANdleBaudrate_E::CAN_BAUD_1M, candleTypes::busTypes_t::USB);
+    Pds  pds(PDS_CAN_ID, candle);
+
+    pds.init();
+
+    auto brakeResistor = pds.attachBrakeResistor(BRAKE_RESISTOR_SOCKET_INDEX);
+
+    // Configuration
+    brakeResistor->setTemperatureLimit(90);
+
+    // Variables for status
+    brakeResistorStatus_S status;
+    f32                   temperature;
+    f32                   temperatureLimit;
+
+    // Read status
+    brakeResistor->getStatus(status);
+    brakeResistor->getTemperature(temperature);
+    brakeResistor->getTemperatureLimit(temperatureLimit);
+
+    // Logging
+    _log.info("Braking resistor");
+    _log.info("Enabled :: [ %s ]", status.ENABLED ? "YES" : "NO");
+    _log.info("Temperature :: [ %.2f °C ]", temperature);
+    _log.info("Temperature limit :: [ %.2f °C ]", temperatureLimit);
+
+    return EXIT_SUCCESS;
+}
--- examples/cpp/pds_example_power_stage.cpp
+++ examples/cpp/pds_example_power_stage.cpp
@@ -1,10 +1,9 @@
 /*
     MAB Robotics
 
-    Power Distribution System Example 2
+    Power Distribution System Example
 
-    Basic submodules operations
-
+    Power stage and braking resistor binding
 */
 
 #include "candle.hpp"
@@ -12,11 +11,10 @@
 
 using namespace mab;
 
-constexpr u16 PDS_CAN_ID = 100;
+constexpr u16 PDS_CAN_ID = 120;
 
-constexpr socketIndex_E ISOLATED_CONVERTER_SOCKET_INDEX = socketIndex_E::SOCKET_1;
-constexpr socketIndex_E POWER_STAGE_SOCKET_INDEX        = socketIndex_E::SOCKET_2;
-constexpr socketIndex_E BRAKE_RESISTOR_SOCKET_INDEX     = socketIndex_E::SOCKET_3;
+constexpr socketIndex_E POWER_STAGE_SOCKET_INDEX    = socketIndex_E::SOCKET_1;
+constexpr socketIndex_E BRAKE_RESISTOR_SOCKET_INDEX = socketIndex_E::SOCKET_3;
 
 int main()
 {
@@ -29,43 +27,37 @@ int main()
 
     pds.init();
 
-    auto isolatedConverter = pds.attachIsolatedConverter(ISOLATED_CONVERTER_SOCKET_INDEX);
-    auto powerStage        = pds.attachPowerStage(POWER_STAGE_SOCKET_INDEX);
-    auto brakeResistor     = pds.attachBrakeResistor(BRAKE_RESISTOR_SOCKET_INDEX);
+    auto powerStage    = pds.attachPowerStage(POWER_STAGE_SOCKET_INDEX);
+    auto brakeResistor = pds.attachBrakeResistor(BRAKE_RESISTOR_SOCKET_INDEX);
 
     if (powerStage == nullptr)
         exit(EXIT_FAILURE);
 
-    if (brakeResistor == nullptr)
-        exit(EXIT_FAILURE);
+    // Configuration
+    powerStage->setTemperatureLimit(90.0f);  // 90 Celsius degrees
+    powerStage->setOcdLevel(25000);          // 25 A OCD level
+    powerStage->setOcdDelay(1000);           // 1 mS delay
 
-    if (isolatedConverter == nullptr)
-        exit(EXIT_FAILURE);
-
-    powerStage->setTemperatureLimit(90.0f);             // 90 Celsius degrees
-    powerStage->setOcdLevel(25000);                     // 25 A OCD level
-    powerStage->setOcdDelay(1000);                      // 1 mS delay
+    // Braking resistor configuration
     powerStage->setBrakeResistorTriggerVoltage(30000);  // 30V DC
-
     powerStage->bindBrakeResistor(brakeResistor->getSocketIndex());
-
-    brakeResistor->setTemperatureLimit(90.0f);      // 90 Celsius degrees
-    isolatedConverter->setTemperatureLimit(70.0f);  // 70 Celsius degrees
-    isolatedConverter->setOcdLevel(4000);           // 4 A OCD level
 
     powerStage->enable();
 
     sleep(1);  // Wait 1 second until power stage is enabled
 
+    // Variables for status
     powerStageStatus_S powerStageStatus = {};
     float              temperature      = 0.0f;
     u32                outputVoltage    = 0;
     s32                outputCurrent    = 0;
 
+    // Read status
     powerStage->getStatus(powerStageStatus);
     powerStage->getOutputVoltage(outputVoltage);
     powerStage->getLoadCurrent(outputCurrent);
 
+    // Logging
     _log.info("Power stage");
     _log.info("Enabled :: [ %s ]", powerStageStatus.ENABLED ? "YES" : "NO");
     _log.info("Over current :: [ %s ]", powerStageStatus.OVER_CURRENT ? "YES" : "NO");
