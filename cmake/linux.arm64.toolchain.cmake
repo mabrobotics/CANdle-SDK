061730969504b19afe089c985335f2fc5d5104ca
PawelKorsak <p.korsak010@gmail.com>
2025-09-30 12:23:39 +0200

    fix: turned off buggy warning of array bounds for ARM64 and HF

--- cmake/linux.arm64.toolchain.cmake
+++ cmake/linux.arm64.toolchain.cmake
@@ -1,24 +1,28 @@
-## Linux ARM64 cross compilation toolchain ##
-# sudo apt-get update
-# sudo apt-get install gcc-aarch64-linux-gnu
-# sudo apt install g++-aarch64-linux-gnu
+# Linux ARM64 cross compilation toolchain ## sudo apt-get update sudo apt-get
+# install gcc-aarch64-linux-gnu sudo apt install g++-aarch64-linux-gnu
 
 message(STATUS "Using ARM64 cross compilation toolchain")
 
-set (CMAKE_SYSTEM_NAME Linux)
-set (CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 # Set cross compile target os
-set (CROSSCOMPILING_TARGET_OS "LINUX")
-set (TARGET_OS "LINUX")
+set(CROSSCOMPILING_TARGET_OS "LINUX")
+set(TARGET_OS "LINUX")
 
-## Set name if not defined as argument ##
-if (NOT TOOLCHAIN_CROSS_TRIPLET)
-	set(TOOLCHAIN_CROSS_TRIPLET "aarch64-linux-gnu") # may also be set to different value using cmake .. -DTOOLCHAIN_CROSS_TRIPLET="aarch64-linux-gnu" ......
-endif ()
+# Set name if not defined as argument ##
+if(NOT TOOLCHAIN_CROSS_TRIPLET)
+  set(TOOLCHAIN_CROSS_TRIPLET "aarch64-linux-gnu"
+  )# may also be set to different value using cmake ..
+   # -DTOOLCHAIN_CROSS_TRIPLET="aarch64-linux-gnu" ......
+endif()
 
-execute_process(COMMAND which "${TOOLCHAIN_CROSS_TRIPLET}-gcc" OUTPUT_VARIABLE CMAKE_C_COMPILER_NEWLINED)
-execute_process(COMMAND which "${TOOLCHAIN_CROSS_TRIPLET}-g++" OUTPUT_VARIABLE CMAKE_CXX_COMPILER_NEWLINED)
+execute_process(COMMAND which "${TOOLCHAIN_CROSS_TRIPLET}-gcc"
+                OUTPUT_VARIABLE CMAKE_C_COMPILER_NEWLINED)
+execute_process(COMMAND which "${TOOLCHAIN_CROSS_TRIPLET}-g++"
+                OUTPUT_VARIABLE CMAKE_CXX_COMPILER_NEWLINED)
+
+add_compile_options(-Wno-array-bounds)
 
 string(STRIP "${CMAKE_C_COMPILER_NEWLINED}" CMAKE_C_COMPILER)
 string(STRIP "${CMAKE_CXX_COMPILER_NEWLINED}" CMAKE_CXX_COMPILER)
--- cmake/linux.armhf.toolchain.cmake
+++ cmake/linux.armhf.toolchain.cmake
@@ -1,24 +1,28 @@
-## Linux ARM64 cross compilation toolchain ##
-# sudo apt update
-# sudo apt install gcc-arm-linux-gnueabihf
-# sudo apt install g++-arm-linux-gnueabihf
+# Linux ARM64 cross compilation toolchain ## sudo apt update sudo apt install
+# gcc-arm-linux-gnueabihf sudo apt install g++-arm-linux-gnueabihf
 
 message(STATUS "Using ARMHF cross compilation toolchain")
 
-set (CMAKE_SYSTEM_NAME Linux)
-set (CMAKE_SYSTEM_PROCESSOR armhf)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR armhf)
 
 # Set cross compile target os
-set (CROSSCOMPILING_TARGET_OS "LINUX")
-set (TARGET_OS "LINUX")
+set(CROSSCOMPILING_TARGET_OS "LINUX")
+set(TARGET_OS "LINUX")
 
-## Set name if not defined as argument ##
-if (NOT TOOLCHAIN_CROSS_TRIPLET)
-	set(TOOLCHAIN_CROSS_TRIPLET "arm-linux-gnueabihf") # may also be set to different value using cmake .. -DTOOLCHAIN_CROSS_TRIPLET="aarch64-linux-gnu" ......
-endif ()
+# Set name if not defined as argument ##
+if(NOT TOOLCHAIN_CROSS_TRIPLET)
+  set(TOOLCHAIN_CROSS_TRIPLET "arm-linux-gnueabihf"
+  )# may also be set to different value using cmake ..
+   # -DTOOLCHAIN_CROSS_TRIPLET="aarch64-linux-gnu" ......
+endif()
 
-execute_process(COMMAND which "${TOOLCHAIN_CROSS_TRIPLET}-gcc" OUTPUT_VARIABLE CMAKE_C_COMPILER_NEWLINED)
-execute_process(COMMAND which "${TOOLCHAIN_CROSS_TRIPLET}-g++" OUTPUT_VARIABLE CMAKE_CXX_COMPILER_NEWLINED)
+execute_process(COMMAND which "${TOOLCHAIN_CROSS_TRIPLET}-gcc"
+                OUTPUT_VARIABLE CMAKE_C_COMPILER_NEWLINED)
+execute_process(COMMAND which "${TOOLCHAIN_CROSS_TRIPLET}-g++"
+                OUTPUT_VARIABLE CMAKE_CXX_COMPILER_NEWLINED)
+
+add_compile_options(-Wno-array-bounds)
 
 string(STRIP "${CMAKE_C_COMPILER_NEWLINED}" CMAKE_C_COMPILER)
 string(STRIP "${CMAKE_CXX_COMPILER_NEWLINED}" CMAKE_CXX_COMPILER)
