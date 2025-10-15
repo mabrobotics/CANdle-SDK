b8eb4ff6208b12832b3d4d525519d1702ae077ba
PawelKorsak <p.korsak010@gmail.com>
2025-10-15 14:00:14 +0200

    feature: yeld of frames

--- candlelib/src/communication_device/candle_frame_adapter.hpp
+++ candlelib/src/communication_device/candle_frame_adapter.hpp
@@ -2,9 +2,12 @@
 
 #include "logger.hpp"
 #include "candle_frame_dto.hpp"
+#include "tsdeque.hpp"
 
+#include <atomic>
 #include <future>
 #include <memory>
+#include <deque>
 
 namespace mab
 {
@@ -21,8 +24,29 @@ namespace mab
         {
         }
 
+        std::vector<CANdleFrame> yeldAccumulatedFrames(const size_t maxCount)
+        {
+            std::vector<CANdleFrame> ret;
+            if (maxCount > m_candleFrameAccumulator.size())
+            {
+                for (size_t i = 0; i < maxCount; i++)
+                {
+                    ret.emplace_back(m_candleFrameAccumulator.pop_front());
+                }
+            }
+            else
+            {
+                const size_t frameAccumulatorSize = m_candleFrameAccumulator.size();
+                for (size_t i = 0; i < frameAccumulatorSize; i++)
+                {
+                    ret.emplace_back(m_candleFrameAccumulator.pop_front());
+                }
+            }
+            return ret;
+        }
+
       private:
         Logger m_log = Logger(Logger::ProgramLayer_E::LAYER_2, "CANDLE_FR_ADAPTER");
-        std::vector<CANdleFrame> m_candleFrameAccumulator;
+        TQueueConcurrent<CANdleFrame> m_candleFrameAccumulator;
     };
 }  // namespace mab
