cdb73375b32144f3ad807b27614942d50c9effd3
PawelKorsak <p.korsak010@gmail.com>
2025-10-21 14:30:06 +0200

    feature: added fail test and timeout in case loop stuck

--- candlelib/src/communication_device/candle_frame_adapter_test.cpp
+++ candlelib/src/communication_device/candle_frame_adapter_test.cpp
@@ -29,19 +29,22 @@ class CandleFrameAdapterTest : public ::testing::Test
     }
 
   protected:
-    static constexpr size_t      CANDLE_FRAME_COUNT = 10;
-    std::vector<std::vector<u8>> mockDataVector;
-    std::counting_semaphore<>    m_binSem = std::counting_semaphore<>(0);
-    std::atomic<bool>            m_stopRequested{false};
+    static constexpr size_t                    CANDLE_FRAME_COUNT = 10;
+    std::vector<std::vector<u8>>               mockDataVector;
+    std::counting_semaphore<>                  m_binSem = std::counting_semaphore<>(0);
+    std::atomic<bool>                          m_stopRequested{false};
+    std::shared_ptr<std::function<void(void)>> m_sync;
 
     void SetUp() override
     {
+        Logger::g_m_verbosity = Logger::Verbosity_E::SILENT;
         for (size_t i = 0; i < CANDLE_FRAME_COUNT; i++)
         {
             std::vector<u8> v;
             v.resize(64, 0);
             std::iota(v.begin(), v.end(), 1);
             mockDataVector.push_back(v);
+            m_sync = std::make_shared<std::function<void(void)>>([this]() { m_binSem.release(); });
         }
     }
     void TearDown() override
@@ -51,9 +54,7 @@ class CandleFrameAdapterTest : public ::testing::Test
 
 TEST_F(CandleFrameAdapterTest, simultaneousReadWrite)
 {
-    auto lamb = std::make_shared<std::function<void(void)>>([this]() { m_binSem.release(); });
-
-    mab::CANdleFrameAdapter cfa(lamb);
+    mab::CANdleFrameAdapter cfa(m_sync);
 
     std::jthread thread(&CandleFrameAdapterTest::mockReadWrite, this, &cfa);
 
@@ -78,4 +79,35 @@ TEST_F(CandleFrameAdapterTest, simultaneousReadWrite)
     }
     thread.request_stop();
     m_binSem.release();
+}
+
+TEST_F(CandleFrameAdapterTest, readerFailError)
+{
+    mab::CANdleFrameAdapter cfa(m_sync);
+
+    std::jthread thread(&CandleFrameAdapterTest::mockReadWrite, this, &cfa);
+
+    std::vector<std::future<std::pair<std::vector<u8>, CANdleFrameAdapter::Error_t>>> futures;
+
+    canId_t canId   = 100;
+    u16     timeout = 10;
+    for (const auto& data : mockDataVector)
+    {
+        futures.push_back(std::async(std::launch::async,
+                                     &CANdleFrameAdapter::accumulateFrame,
+                                     &cfa,
+                                     canId++,
+                                     data,
+                                     timeout));
+    }
+    CANdleFrameAdapter::Error_t lastResult = CANdleFrameAdapter::Error_t::OK;
+
+    for (auto& future : futures)
+    {
+        auto result = future.get();
+        thread.request_stop();
+        m_binSem.release();
+        lastResult = result.second;
+    }
+    EXPECT_EQ(CANdleFrameAdapter::Error_t::READER_TIMEOUT, lastResult);
 }
