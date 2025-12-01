be3f8e31e2202c4b9a31faaf99011abc35f56222
Jakub Matyszczak <jakub.matyszczak@mabrobotics.pl>
2025-12-01 12:28:11 +0100

    feat: helpers

--- commons/shared_data/manufacturer_data.hpp
+++ commons/shared_data/manufacturer_data.hpp
@@ -21,15 +21,36 @@ namespace mab
         CANDLE           = 0xA1,
         CANDLE_HAT       = 0xA2
     };
+    inline char* deviceTypeToCstring(deviceType_E type)
+    {
+        switch (type)
+        {
+            case mab::deviceType_E::MD80:
+                return (char*)"md80";
+            case mab::deviceType_E::MD20:
+                return (char*)"md20";
+            case mab::deviceType_E::MD80HV:
+                return (char*)"md80hv";
+            case mab::deviceType_E::CANDLE:
+                return (char*)"candle";
+            case mab::deviceType_E::CANDLE_HAT:
+                return (char*)"candlehat";
+            case mab::deviceType_E::PDS:
+                return (char*)"candlehat";
+            default:
+                return (char*)"UNKNOWN";
+        }
+    }
 
 #pragma pack(push, 1)
     struct manufacturerData_S
     {
-        u32          CRC32;
-        u16          version;
+        u32          CRC32;    // TODO: placeholder
+        u16          version;  // manufacturerData format version
         deviceType_E deviceType;
-        u8           deviceRevision;
-        u8           batchCode[24];
+        u8           deviceRevision;  // 10 -> HW1.0, 32 -> HW3.2
+        u8           prodDate[6];     // ddmmyy format
+        u8           batchCode[24];   // TODO: ASM production code placeholder
 
         inline bool isEmpty() const
         {
