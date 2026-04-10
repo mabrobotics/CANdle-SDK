54780380b188bdd1a082d2b4620232c2c7f3732c
Błażej Sowa <bsowa123@gmail.com>
2026-04-10 16:14:51 +0200

    Fix patch for newer CandleSDK

--- candletool/include/configHelpers.hpp
+++ candletool/include/configHelpers.hpp
@@ -20,7 +20,7 @@ namespace mab
         GetModuleFileName(NULL, path, 256);
         return std::filesystem::path(path).remove_filename().string() + std::string("config\\");
 #else
-        return std::filesystem::path(DEFAULT_CANDLETOOL_CONFIG_DIR) + std::string("/");
+        return std::filesystem::path(DEFAULT_CANDLETOOL_CONFIG_DIR "/");
 #endif
     }
 
