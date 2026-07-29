270e085af99c51303373a3fd5e6a3315b3786f4d
MilkoRoszko <meimek13@gmail.com>
2026-07-29 14:53:42 +0200

    fix: windows dependencies

--- launch/buildForWindows.sh
+++ launch/buildForWindows.sh
@@ -9,7 +9,7 @@ cd ${base_dir}
 mkdir $BUILD_DIRECTORY -p
 cd $BUILD_DIRECTORY
 chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
-cmake .. -DCMAKE_TOOLCHAIN_FILE=${base_dir}/cmake/windows.x86_64.gcc.toolchain.cmake -DPKG_CONFIG_EXECUTABLE=/usr/bin/pkg-config
+cmake .. PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/lib/pkgconfig cmake .. -DCMAKE_TOOLCHAIN_FILE=${base_dir}/cmake/windows.x86_64.gcc.toolchain.cmake -DPKG_CONFIG_EXECUTABLE=/usr/bin/pkg-config
 chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
 make -j
 chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
