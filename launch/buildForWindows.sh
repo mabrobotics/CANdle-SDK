ce59f367b77bb7992c75d8dfb268567a738be36b
MilkoRoszko <meimek13@gmail.com>
2026-07-29 14:04:21 +0200

    fix: windows

--- launch/buildForWindows.sh
+++ launch/buildForWindows.sh
@@ -9,7 +9,7 @@ cd ${base_dir}
 mkdir $BUILD_DIRECTORY -p
 cd $BUILD_DIRECTORY
 chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
-cmake .. -DCMAKE_TOOLCHAIN_FILE=${base_dir}/cmake/windows.x86_64.gcc.toolchain.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=${base_dir}/cmake/windows.x86_64.gcc.toolchain.cmake -DPKG_CONFIG_EXECUTABLE=/usr/bin/pkg-config
 chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
 make -j
 chmod -R a+rw ${base_dir}/${BUILD_DIRECTORY}
