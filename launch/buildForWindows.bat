da3f78300fc550043788b296a20ec6fb07ae438a
MilkoRoszko <meimek13@gmail.com>
2026-08-27 12:34:32 +0200

    fix: restored buildForWindows.bat file

--- launch/buildForWindows.bat
+++ launch/buildForWindows.bat
@@ -1,5 +1,4 @@
 @echo off
-setlocal EnableDelayedExpansion
 if not exist C:\w64devkit\bin\gcc.exe (
     echo Downloading w64devkit...
     curl -OL https://github.com/skeeto/w64devkit/releases/download/v1.22.0/w64devkit-1.22.0.zip
@@ -10,23 +9,8 @@ if not exist C:\w64devkit\bin\gcc.exe (
     echo Rerun the script to build.
     exit /b 0
 )
-set "NSIS_DIR=C:\Program Files (x86)\NSIS"
-if not exist "!NSIS_DIR!\makensis.exe" (
-    if not exist "C:\NSIS\makensis.exe" (
-        echo NSIS not found. Downloading NSIS installer...
-        curl -L -o nsis-setup.exe "https://sourceforge.net/projects/nsis/files/NSIS%203/3.10/nsis-3.10-setup.exe/download"
-        echo Installing NSIS silently...
-        nsis-setup.exe /S
-        del nsis-setup.exe /Q
-        echo NSIS installed successfully.
-    ) else (
-        set "NSIS_DIR=C:\NSIS"
-    )
-)
 echo w64devkit found!
 path|find /i "w64devkit"    >nul || set path=%path%;C:\w64devkit\bin
-path|find /i "NSIS"         >nul || set "PATH=%PATH%;!NSIS_DIR!"
-
 echo Build files will be stored in %0\..\..\build
 mkdir %0\..\..\build
 set currentdir="%cd%"
@@ -35,4 +19,3 @@ cmake -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release ..
 make package -j4
 cd %currentdir%
 exit /b 0
-
